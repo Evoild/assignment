e3c4a17a20b8afaee7fa05e6a95026f18d640df3
马逸宁 <14628093+evoild@user.noreply.gitee.com>
2025-10-12 16:09:50 +0800

    default

---
+++ 数字图像处理实验专题/Experiment-2/code/utils.py
@@ -0,0 +1,83 @@
+import torch
+import sys
+import numpy as np
+from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+def pil_to_np(img_PIL):
+    '''
+    From W x H x C [0...255] to C x W x H [0..1]
+    '''
+    ar = np.array(img_PIL)
+
+    if len(ar.shape) == 3:
+        ar = ar.transpose(2,0,1)
+    else:
+        ar = ar[None, ...]
+
+    return ar.astype(np.float32) / 255.
+
+def np_to_pil(img_np): 
+    '''
+    From C x W x H [0..1] to  W x H x C [0...255]
+    '''
+    ar = np.clip(img_np*255,0,255).astype(np.uint8)
+    
+    if img_np.ndim == 2:
+        ar = ar
+    else:
+        ar = ar.transpose(1, 2, 0)
+
+    return Image.fromarray(ar)
+
+def np_to_torch(img_np):
+    '''Converts image in numpy.array to torch.Tensor.
+    From C x W x H [0..1] to  C x W x H [0..1]
+    '''
+    return torch.from_numpy(img_np)[None, :]
+
+def torch_to_np(img_var):
+    '''Converts an image in torch.Tensor format to np.array.
+    From 1 x C x W x H [0..1] to  C x W x H [0..1]
+    '''
+    return img_var.detach().cpu().numpy()[0]
+
+def images_np_load(folder_path, plot=False):
+    images = []
+    for filename in os.listdir(folder_path):
+        if filename.lower().endswith(('.jpg', '.png')):
+            img_path = os.path.join(folder_path, filename)
+            img = pil_to_np(Image.open(img_path))
+            print(f"已加载: {filename}, 尺寸: {img.shape}")
+            images.append(img)
+            if plot:
+                image_plot(img)
+    return images
+
+def images_np_save(images, folder_path):
+    idx = 0
+    for img in images:
+        img = np_to_pil(img)
+        img.save(folder_path + f"{idx}.jpg")
+        idx += 1
+
+def image_plot(image):
+    ## modify the shape to H*W*C
+    if image.shape[0] == 3:
+        img = np.transpose(image, [1, 2, 0])
+    else:
+        img = image
+
+    if img.ndim == 3:
+        plt.imshow(img)
+    elif img.ndim == 2:
+        plt.imshow(img, cmap="gray")
+
+    plt.axis('off')
+    plt.show()
+
+def images_plot(images):
+    for image in images:
+        image_plot(image)
